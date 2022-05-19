e270e09c62657f69fd3f75e87e3484212f5b3867
mezermize <103459165+mezermize@users.noreply.github.com>
2022-05-19 15:25:54 +0100

    xand

--- Projecto_3_IA_ALUNOS/src/simulator/Decision.java
+++ Projecto_3_IA_ALUNOS/src/simulator/Decision.java
@@ -1,7 +1,8 @@
 package simulator;
 
+import java.util.List;
 import java.util.Random;
-
+import net.sourceforge.jFuzzyLogic.FIS;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instance;
@@ -9,9 +10,61 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Decision {
+		
 	public static void main(String[] args) {
-		// Prepare dataset from file
+		
+		//decision();
+		try {
+		DataSource source = new DataSource("mushroom.arff");
+		Instances dataset = source.getDataSet();
+		// definir atributo target
+		dataset.setClassIndex(dataset.numAttributes() - 1);
 
+		// Generated model
+		J48 classifier = new J48();
+		classifier.buildClassifier(dataset);
+		
+		Simulator simulador = new Simulator();
+		while(true) {
+			
+		  String[] atributes = simulador.getMushroomAttributes();
+			NewInstances instance = new NewInstances(dataset) ;
+			
+			System.out.println(atributes + "---" + instance);
+			
+			//load FCL FILE
+			fuzzao();
+		}
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+	}
+	
+	
+	private static void fuzzao() {
+		// Prepare dataset from file
+		// Load from 'FCL' file
+		String fileName = "codumelos.fcl";
+		FIS fis = FIS.load(fileName, true);
+		// Error while loading?
+		if (fis == null) {
+			System.err.println("Can't load file: '" + fileName + "'");
+			return;
+		}
+
+		// Set inputs
+		fis.setVariable("distance", 8);
+//		fis.setVariable("danger", 5);
+		fis.setVariable("sensor", 90);
+		// Evaluate
+		fis.evaluate();
+		// Print ruleSet
+		System.out.println("Actione: " + fis.getVariable("rotation").defuzzify());
+	}
+
+	private static void decision() {
 		try {
 			DataSource source = new DataSource("mushroom.arff");
 			Instances dataset = source.getDataSet();
@@ -34,28 +87,27 @@ public class Decision {
 
 			// Test a new instance
 			NewInstances ni = new NewInstances(dataset);
-			String[] values1 = { "anise" , "brown" , "bell"  };
-			String[] values2 = { "fishy" , "buff", "convex" };
-			
+			String[] values1 = { "anise", "brown", "bell" };
+			String[] values2 = { "fishy", "buff", "convex" };
+
 			ni.addInstance(values1);
 			ni.addInstance(values2);
-			
+
 			Instances test_dt = ni.getDataset();
 			System.out.println("ActualClass \t PredictedClass");
-			
+
 			for (int i = 0; i < test_dt.numInstances(); i++) {
 				Instance inst = test_dt.instance(i);
 				String actual = inst.stringValue(inst.numAttributes() - 1);
-				
+
 				double predict = classifier.classifyInstance(inst);
 				String pred = test_dt.classAttribute().value((int) (predict));
-				
+
 				System.out.println(actual + " \t " + pred);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 }
